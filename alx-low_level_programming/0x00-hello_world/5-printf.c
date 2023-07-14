7f57387f9bec35b162f6f4b27a91e7f52ae35433
sams-ddev <sams.alxstudent@gmail.com>
2023-07-14 19:04:05 +0000

    wrote 5-printf.c

---
+++ alx-low_level_programming/0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#inlclude <stdio.h>
+
+/**
+ * main - Write a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
