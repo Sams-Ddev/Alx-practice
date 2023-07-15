4813ffc9bc6f79f92e5791d8bcf747df52fee90a
sams-ddev <sams.alxstudent@gmail.com>
2023-07-15 13:57:36 +0000

    wrote _printf.c

---
+++ _printf/_printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	var_list args;
+	int count = 0;
+
+	var_start(args, format);
+
+	while
+}
---
+++ _printf/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+int _printstring(char *str);
+
+#endif	/* main.h file*/
