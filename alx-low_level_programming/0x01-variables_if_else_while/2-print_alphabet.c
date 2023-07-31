647095a86d45eb545a7ce915aa4c4ae86c0c5925
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 04:10:58 +0000

    wrote the file 2-print_alphabet.c

---
+++ alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ */
+
+
