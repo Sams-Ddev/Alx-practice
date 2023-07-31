9c7e28f3611a4ab8de266bf2673ac66da939511c
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 04:02:04 +0000

    corrections to 1-digitss corrections

--- alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
+++ alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
@@ -28,6 +28,10 @@ int main(void)
 		printf("Last digit od %d is %d and is 0\n", n, r);
 	else if  (r < 6 && r != 0)
 		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, r);
-
+	else if (r < 0)
+	{
+		r = -r
+		printf("The remainder %d is a negative", r);
+	}
 	return (0);
 }
