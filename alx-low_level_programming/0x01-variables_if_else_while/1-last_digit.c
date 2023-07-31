7857196297f337fd3bd5788e998acd15172f7dfc
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 03:32:38 +0000

    wrote  program will assign a random number to the variable n each time it is executed

---
+++ alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * This program will assign a random number to the variable
+ * n each time it is executed. 
+ *
+ * The Ccompleted the source code in order to print
+ * the last digit of the number stored in the variable n.
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	int r = n % 10;
+
+	if (r > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, r);
+	else if (r == 0)
+		printf("Last digit od %d is %d and is 0\n", n, r);
+	else (r < 6 && r != 0)
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, r);
+
+	return (0);
+}
