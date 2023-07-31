4f23d659971dd4b794d4bb90ed5e69ef07bb5ecd
sams-ddev <sams.alxstudent@gmail.com>
2023-07-31 03:01:57 +0000

    wrote a This program will assign a random number to the variable n each time it is executed.

---
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * Program - 
+ *
+ * This program will assign a random number to the variable 
+ * n each time it is executed.
+ *
+ * The Complete the source code in order to print whether 
+ * the number stored in the variable n is positive or negative.
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	if (n < 0)
+		printf("The number %d is negative", n);
+	else
+		printf("The number %d is a positve", n);
+
+	return (0);
+}
