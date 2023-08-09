ac4eda899a3db479d43d3bce38755c200616a55a
sams-ddev <sams.alxstudent@gmail.com>
2023-08-09 17:51:43 +0000

    wrote a program that demonstrate the use of argc and argv[]

---
+++ alx-low_level_programming/0x0A-argc_argv/argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * The program demonstates the use of argc and arrgv[]
+ * and prints out the values each is holding
+ *
+ * Return - EXIT_SUCCESS on completion
+ */
+
+int main(int argc, char *argv[])
+{
+	print("The value of argc/t= %d", argc);
+	return EXIT_SUCCESS
+}
