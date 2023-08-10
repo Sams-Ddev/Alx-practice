2f4e44a9c1856e5ced152a8bbc4590b6bb0f9bbb
sams-ddev <sams.alxstudent@gmail.com>
2023-08-10 16:42:19 +0000

    betty correction on 0-whatsmyname

--- alx-low_level_programming/0x0A-argc_argv/0-whatsmyname.c
+++ alx-low_level_programming/0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,15 @@
 
 /**
  * main - prints its name, followed by a new line.
+ * @argc: The argument count
+ * @argv: the char array stored
  *
- * Return - EXIT_SUCCESS
+ * Return: EXIT_SUCCESS
 */
+
 int main(int argc, char *argv[])
 {
+	_putchar('%d', argc);
 
+	return (EXIT_SUCCESS);
 }
