0580d38caa06281225546e391f183cfe19a15fe4
sams-ddev <sams.alxstudent@gmail.com>
2023-08-16 21:54:12 +0000

    testing function gegtline() parameters

--- simple_shell/Everything/Readline.c
+++ simple_shell/Everything/Readline.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
+/**
+ * program that prints "$ ", wait for the user to enter
+ * a command, prints it on the next line
+ *
+ * functions - man getline().
+ */
 
+int main(void)
+{
+	printf("$ ");
+	getline();
+}
