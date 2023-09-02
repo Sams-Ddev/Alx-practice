965be343527efaae3a5a6369f57cbc710d60474d
sams-ddev <sams.alxstudent@gmail.com>
2023-09-02 22:14:13 +0000

    wrote the file 2-OR.c

---
+++ alx-low_level_programming/bit_manipulation/2-OR.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * OR(|) - Its a bitwise operation, take two operands
+ * and make a | operation.
+ * The result is true(1) when any of the operands is 1,
+ * otherwise zero.
+ */
+
+int main(void)
+{
+	
+	return (result);
+}
