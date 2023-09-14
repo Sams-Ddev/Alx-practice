d522965d38cc70a56a5c1ea7ad1f84b12bace5ee
sams-ddev <sams.alxstudent@gmail.com>
2023-09-14 20:01:07 +0000

    dprint()

---
+++ simple_shid/errtin/dprint.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+  int fd = open("myfile.txt", O_WRONLY);
+  if (fd == -1) {
+    perror("open");
+    return 1;
+  }
+
+  dprintf(fd, "Hello, world!\n");
+
+  close(fd);
+
+  return 0;
+}
+
