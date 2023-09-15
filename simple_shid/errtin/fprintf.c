35fe3b88cd1560eb74c57bf064a5b0770ee00aa3
sams-ddev <sams.alxstudent@gmail.com>
2023-09-15 06:56:06 +0000

    wrote the func fprint.c

--- simple_shid/errtin/fprintf.c
+++ simple_shid/errtin/fprintf.c
@@ -10,8 +10,17 @@ int main(void)
 
 	fprintf(file, "Hello, world!\n");
 
-	fclose(fd);
+	fclose(file);
 
 	retyurn (0);
 
 }
+
+/*In this example, the fprintf() function is used to write the string "Hello, world!"
+ * to the file output. txt . The fopen() function is used to open the file output.txt
+ * for writing. The fclose() function is used to close the file output.txt.
+ * The fprintf() function can be used to format output in a variety of ways.
+ * The format string can contain conversion specifiers, which are used
+ * to indicate the type of data that is being formatted.
+ * For example, the following conversion specifiers can be used to format output:
+ */
